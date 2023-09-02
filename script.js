e85121b0d1593de2445474940501035efba9d0b5
uditgiri01 <114456247+uditgiri01@users.noreply.github.com>
2023-09-02 18:17:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,128 @@
-let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-let timeRef = document.querySelector(".timer-display");
-let int = null;
+const search = document.getElementById('search'),
+      submit = document.getElementById('submit'),
+      random = document.getElementById('random'),
+      mealsEl = document.getElementById('meals'),
+      resultHeading = document.getElementById('result-heading'),
+      single_mealEl = document.getElementById('single-meal');
 
-document.getElementById("start-timer").addEventListener("click", () => {
-    if(int !== null) {
-        clearInterval(int);
+// Search meal and fetch from API
+function searchMeal(e) {
+    e.preventDefault();
+
+    // Clear single meal
+    single_mealEl.innerHTML = '';
+
+    //Get search term
+    const term = search.value;
+
+    //check for empty
+    if(term.trim()) {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+
+                if(data.meals === null) {
+                    resultHeading.innerHTML = `<p>Sorry we can't find your search results. Try something else!<p>`;
+                }else {
+                    mealsEl.innerHTML = data.meals.map(
+                        meal => `
+                        <div class="meal">
+                            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                            <div class="meal-info" data-mealID="${meal.idMeal}">
+                            <h3>${meal.strMeal}</h3>
+                            </div>
+                        </div>
+                        `
+                    )
+                    .join('');
+                }
+            });
+
+            //Clear search text
+            search.value = '';
+
+    }else {
+        alert('Please enter a search term');
     }
-    int = setInterval(displayTimer, 10);
-});
 
-document.getElementById("pause-timer").addEventListener("click", () => {
-    clearInterval(int);
-});
+}
 
-document.getElementById("reset-timer").addEventListener("click", () => {
-    clearInterval(int);
-    [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-    timeRef.innerHTML = "00 : 00 : 00 : 000 ";
-}); 
+//Fetch meal by ID
+function getMealById(mealID) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(res => res.json())
+    .then(data => {
+        const meal = data.meals[0];
 
-function displayTimer() {
-    milliseconds += 10;
-    if(milliseconds == 1000) {
-        milliseconds = 0;
-        seconds++;
-        if(seconds == 60) {
-            seconds = 0;
-            minutes++;
-            if(minutes == 60) {
-                minutes = 0;
-                hours++;
-            }
+        addMealToDom(meal);
+    });
+}
+
+//Fetch random meal from API
+function getRandomMeal() {
+    //Clear meals and heading
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then(res => res.json())
+    .then(data => {
+        const meal = data.meals[0];
+
+        addMealToDom(meal)
+    });
+}
+
+// Add meal to DOM
+function addMealToDom(meal) {
+    const ingredients = [];
+
+    for(let i = 1; i <= 20; i++) {
+        if(meal[`strIngredient${i}`]) {
+            ingredients.push(
+                `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+            );
+        } else {
+            break;
         }
     }
 
-    let h = hours < 10 ? "0" + hours : hours;
-    let m = minutes < 10 ? "0" + minutes : minutes;
-    let s = seconds < 10 ? "0" + seconds : seconds;
-    let ms = 
-        milliseconds < 10
-        ? "00" + milliseconds
-        : milliseconds < 100
-        ? "0" + milliseconds
-        : milliseconds;
-
-    timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
-
+    single_mealEl.innerHTML = `
+        <div class="single-meal">
+            <h1>${meal.strMeal}</h1>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+            <div class="single-meal-info">
+            ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+            ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+            </div>
+            <div class="main">
+                <p>${meal.strInstructions}</p>
+                <h2>Ingredients</h2>
+                <ul>
+                    ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+                </ul>
+            </div>
+        </div>
+    `;
 }
+
+// Event listeners
+submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
+
+mealsEl.addEventListener('click', e => {
+    const mealInfo = e.path.find(item => {
+        if(item.classList) {
+            return item.classList.contains('meal-info');
+        }else{
+            false;
+        }
+    });
+
+    if(mealInfo) {
+        const mealID = mealInfo.getAttribute('data-mealid');
+        getMealById(mealID);
+    }
+});
